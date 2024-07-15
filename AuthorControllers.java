fa5ee22baba4a9c11c1babf0457c02f43bd6928f
lilli2203 <mailyourlilli@gmail.com>
2024-07-16 00:34:11 +0530

    chanegs

--- AuthorControllers.java
+++ AuthorControllers.java
@@ -8,34 +8,71 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/author")
-public class AuthorControllers {
-    AuthorService authorService;
+public class AuthorController {
+    private final AuthorService authorService;
 
-    AuthorControllers(AuthorService authorService) {
+    AuthorController(AuthorService authorService) {
         this.authorService = authorService;
     }
 
-    // Get All Authors
-    @GetMapping
+
+        @GetMapping
     public List<Author> getAllAuthors() {
         return authorService.getAllAuthors();
     }
 
-    // Create an Author
+    @GetMapping("/{id}")
+    public Author getAuthorById(@PathVariable("id") Long id) {
+        return authorService.getAuthorById(id);
+    }
+
     @PostMapping
     public Author createAuthor(@RequestBody Author author) {
         return authorService.createAuthor(author);
     }
 
-    // Update an Author
     @PutMapping("/{id}")
     public Author updateAuthor(@PathVariable("id") Long id, @RequestBody Author author) {
         return authorService.updateAuthor(id, author);
     }
 
-    // Delete an Author
     @DeleteMapping("/{id}")
     public void deleteAuthor(@PathVariable("id") Long id) {
         authorService.deleteAuthor(id);
+    }
+
+    @GetMapping("/search")
+    public List<Author> searchAuthorsByName(@RequestParam("name") String name) {
+        return authorService.searchAuthorsByName(name);
+    }
+
+    @GetMapping("/nationality/{nationality}")
+    public List<Author> getAuthorsByNationality(@PathVariable("nationality") String nationality) {
+        return authorService.getAuthorsByNationality(nationality);
+    }
+
+    @PostMapping("/batchCreate")
+    public List<Author> batchCreateAuthors(@RequestBody List<Author> authors) {
+        return authorService.batchCreateAuthors(authors);
+    }
+
+    @PutMapping("/batchUpdate")
+    public List<Author> batchUpdateAuthors(@RequestBody List<Author> authors) {
+        return authorService.batchUpdateAuthors(authors);
+    }
+
+    @DeleteMapping("/batchDelete")
+    public void batchDeleteAuthors(@RequestBody List<Long> authorIds) {
+        authorService.batchDeleteAuthors(authorIds);
+    }
+
+    @GetMapping("/export")
+    public void exportAuthorsToCSV() {
+        authorService.exportAuthorsToCSV();
+    }
+
+    @PostMapping("/import")
+    public List<Author> importAuthorsFromCSV(@RequestBody String csvData) {
+        return authorService.importAuthorsFromCSV(csvData);
     }
 }
